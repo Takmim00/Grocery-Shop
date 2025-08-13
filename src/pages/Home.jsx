0e5990ed130207a5ca6e7c1b178952c24a26ce22
Takmim00 <takmimm@gmail.com>
2025-08-13 20:43:02 +0600

    edit picks section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../component/Banner";
 import Category from "../component/Category";
+import EditorPicks from "../component/EditorPicks";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Category/>
+      <EditorPicks/>
     </div>
   );
 };
