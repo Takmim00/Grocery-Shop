98c42a4a2a9df05492b6e6d6cdf8648a1ed6cea1
Takmim00 <takmimm@gmail.com>
2025-08-14 20:11:46 +0600

    login and signup page hide navabr footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../component/Navbar";
 
 const Main = () => {
+  const location = useLocation();
+
+  const hideRoutes = ["/login", "/signup"];
+  const hideLayout = hideRoutes.includes(location.pathname);
   return (
     <div>
-      <Navbar/>
+      {!hideLayout && <Navbar />}
       <Outlet />
     </div>
   );
